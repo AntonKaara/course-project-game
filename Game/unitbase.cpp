eae2ef24f6a04bf806c6dfc8e2cf83524e215399
Anton Kaarakainen <anton.kaarakainen@tuni.fi>
2019-11-20 20:45:30 +0200

    Added UnitBase member functions

--- Game/unitbase.cpp
+++ Game/unitbase.cpp
@@ -17,8 +17,60 @@ UnitBase::UnitBase(const std::shared_ptr<Course::iGameEventHandler > &eventhandl
 
 }
 
-void UnitBase::doSpecialAction() {
+std::string UnitBase::getType() const {
+    return "Generic unit"; // Override in child classes
+}
 
+void UnitBase::doSpecialAction() {
+    // Override in child classes
+}
+
+void UnitBase::setName(const std::string name) {
+    unitName_ = name;
+}
+
+void UnitBase::setHealth(const int amount) {
+    healthPoints_ = amount;
+}
+
+void UnitBase::setMovement(const double amount) {
+    movementPoints_ = amount;
+}
+
+void UnitBase::setRange(const int amount) {
+    range_ = amount;
+}
+
+void UnitBase::setDamage(const int amount) {
+    damage_ = amount;
+}
+
+std::string UnitBase::getName() {
+    return unitName_;
+}
+
+int UnitBase::getHealth() {
+    return healthPoints_;
+}
+
+double UnitBase::getMovement() {
+    return movementPoints_;
+}
+
+int UnitBase::getRange() {
+    return range_;
+}
+
+int UnitBase::getDamage() {
+    return damage_;
+}
+
+void UnitBase::changeHealth(int amount) {
+    healthPoints_ =  healthPoints_ + amount;
+}
+
+void UnitBase::changeMovement(int amount) {
+    movementPoints_ = movementPoints_ + amount;
 }
 
 } // namespace Aeta
--- Game/unitbase.h
+++ Game/unitbase.h
@@ -17,7 +17,33 @@ public:
              const int &tilespaces=1, const Course::ResourceMap &cost={},
              const Course::ResourceMapDouble &efficiency={});
 
-    void doSpecialAction() override;
+    virtual std::string getType() const override;
+    virtual void doSpecialAction() override;
+
+    virtual void setName(const std::string name);
+    virtual void setHealth(const int amount);
+    virtual void setMovement(const double amount);
+    virtual void setRange(const int amount);
+    virtual void setDamage(const int amount);
+
+    virtual std::string getName();
+    virtual int getHealth();
+    virtual double getMovement();
+    virtual int getRange();
+    virtual int getDamage();
+
+    virtual void changeHealth(int amount);
+    virtual void changeMovement(int amount);
+
+
+private:
+
+    std::string unitName_;
+    int healthPoints_;
+    double movementPoints_;
+    int range_;
+    int damage_;
+
 
 };
 
